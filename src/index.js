1f5665062f2f1d08572d57f570619194b399366f
LuisCasas <sc.luis@gmail.com>
2018-09-14 22:03:35 +0100

    Adding react state functionality to Search component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const api = require('./config/config.js');
+const API_KEY = api['API_KEY'];
 
 // Create new component for produce some HTML
 const App = () => {
-    return <div>Hello React!</div>;
+    return (
+        <div>
+        <SearchBar />
+     </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
