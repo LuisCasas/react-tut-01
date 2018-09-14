9102c9b5b68947e5884bfdddc4ae42933c82c4c2
LuisCasas <sc.luis@gmail.com>
2018-09-14 22:29:06 +0100

    Adding Ajax call functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const api = require('./config/config.js');
-const API_KEY = api['API_KEY'];
+const API_KEY = api.default.API_KEY;
 
 // Create new component for produce some HTML
-const App = () => {
-    return (
-        <div>
-        <SearchBar />
-     </div>
-    );
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'skate'}, (videos) => {
+            this.state({ videos }); // equivelant of {videos: videos}
+        });        
+    }
+
+    render(){
+        return (
+            <div>
+            <SearchBar />
+        </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
