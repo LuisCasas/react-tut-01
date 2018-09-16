8f9c0388624d30177c7ec9156fd81925c0081370
LuisCasas <sc.luis@gmail.com>
2018-09-16 17:30:58 +0100

    Adding search bar functionality and complete the YouTube Search web application

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -19,19 +20,26 @@ class App extends Component{
             selectedVideo: null 
         };
 
-        YTSearch({key: API_KEY, term: 'skate'}, (videos) => {
-           // this.setState({ videos }); // equivelant of {videos: videos}
-           this.setState({ 
-                videos: videos,
-                selectedVideo: videos[0]
-            }); 
-        });        
+        this.videoSerch('snowboard');
+    }
+
+    videoSerch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            // this.setState({ videos }); // equivelant of {videos: videos}
+            this.setState({ 
+                 videos: videos,
+                 selectedVideo: videos[0]
+             }); 
+         });  
     }
 
     render(){
+
+        const videoSearch = _.debounce((term) => {this.videoSerch(term)}, 300);
+
         return (
             <div>
-            <SearchBar />
+            <SearchBar onSearchTermChange={videoSearch} />
             <VideoDetails video={this.state.selectedVideo} />
             <VideoList
                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
